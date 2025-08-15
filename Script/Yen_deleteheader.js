e528119a707623c845445b694e300a0abe3f8a19
Khổng Mạnh Yên💢 <manhyenver2@gmail.com>
2025-08-15 18:50:06 +0700

    Create Yen_deleteheader.js

---
+++ Script/Yen_deleteheader.js
@@ -0,0 +1,6 @@
+//# 📌 Ghi chú: File module này được tạo riêng bởi KHỔNG MẠNH YÊN. 
+//# Mong bạn không chỉnh sửa hay chia sẻ lại nếu chưa có sự cho phép. Xin cảm ơn ❤️
+const version = 'V1.0.2';
+
+
+function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
